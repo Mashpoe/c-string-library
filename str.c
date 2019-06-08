6a257800661ff4eb40e702332f47192b8aa644e5
Mashpoe <mashpoesemail@gmail.com>
2019-06-08 13:27:10 -0600

    lowercase & include correct header

--- str.c
+++ str.c
@@ -2,73 +2,59 @@
 //  str.c
 //
 //  Created by Mashpoe on 2/28/19.
-//  Copyright © 2019 Mashpoe. All rights reserved.
 //
 
-#include "str2.h"
+#include "str.h"
 #include <stdio.h>
 #include <string.h>
 
 typedef struct string_data string_data;
 
 struct string_data {
-	str_size alloc; // stores the number of bytes allocated
-	str_size length;
+	size_t alloc; // stores the number of bytes allocated
+	size_t length;
 	char buff[];
 };
 
-string_data* STRING_ALLOC(str_size length) {
+string_data* string_alloc(size_t length) {
 	string_data* s_data = malloc(sizeof(string_data) + length + 1); // plus 1 for null terminator
 	s_data->alloc = length+1;
 	s_data->length = length;
 	return s_data;
 }
 
-string_data* STRING_DATA(string s) {
+string_data* string_get_data(string s) {
 	return (string_data*)&s[-sizeof(string_data)];
 }
 
-string STRING_CREATE(const char* str) {
+string string_create(const char* str) {
 	string_data* s_data = NULL;
 	
 	if (str != NULL) {
-		str_size str_length = strlen(str);
-		s_data = STRING_ALLOC(str_length);
+		size_t str_length = strlen(str);
+		s_data = string_alloc(str_length);
 		memcpy(&s_data->buff, str, str_length);
 		s_data->buff[str_length] = '\0';
 	} else {
-		s_data = STRING_ALLOC(0); // will only allocate enough for a null terminator
+		s_data = string_alloc(0); // will only allocate enough for a null terminator
 		s_data->buff[0] = '\0';
 	}
 	
 	return s_data->buff;
 }
 
-string_data* STRING_REALLOC(string_data* s_data) {
-	str_size new_alloc = s_data->alloc * 2;
-	
-	string_data* new_s_data = malloc(sizeof(string_data) + new_alloc);
-	
-	new_s_data->alloc = new_alloc;
-	new_s_data->length = s_data->length;
-	
-	memcpy(new_s_data->buff, s_data->buff, s_data->length);
-	
-	free(s_data);
-	
-	return new_s_data;
-}
-
-bool STRING_HAS_SPACE(string_data* s_data) {
+bool string_has_space(string_data* s_data) {
 	// allocate based on s_data->length + 1 to account for the null terminator
 	return s_data->alloc - (s_data->length + 1) > 0;
 }
 
-void STRING_ADD_CHAR(string* s, char c) {
-	string_data* s_data = STRING_DATA(*s);
+void string_add_char(string* s, char c) {
+	string_data* s_data = string_get_data(*s);
 	
-	if (!STRING_HAS_SPACE(s_data)) {
-		s_data = STRING_REALLOC(s_data);
+	if (!string_has_space(s_data)) {
+		size_t new_alloc = s_data->alloc * 2;
+		s_data = realloc(s_data, sizeof(string_data) + new_alloc);
+		s_data->alloc = new_alloc;
 	}
 	s_data->buff[s_data->length++] = c;
 	s_data->buff[s_data->length] = '\0'; // add the new null terminator
@@ -76,22 +62,18 @@ void STRING_ADD_CHAR(string* s, char c) {
 	*s = s_data->buff;
 }
 
-void STRING_ADD(string* s, const char* str) {
-	string_data* s_data = STRING_DATA(*s);
+void string_add(string* s, const char* str) {
+	string_data* s_data = string_get_data(*s);
 	
-	str_size str_length = strlen(str);
+	size_t str_length = strlen(str);
 	
-	str_size new_length = s_data->length + str_length;
+	size_t new_length = s_data->length + str_length;
 	
 	// make sure there is enough room for new characters and null terminator
 	if (s_data->alloc <= new_length + 1) {
-		string_data* new_s_data = STRING_ALLOC(new_length);
-		
-		memcpy(new_s_data->buff, s_data->buff, s_data->length - 1); // copy characters
-		
-		free(s_data);
-		
-		s_data = new_s_data;
+		s_data = realloc(s_data, sizeof(string_data) + new_length);
+		s_data->length = new_length;
+		s_data->alloc = new_length + 1;
 	}
 	
 	// copy str chars
@@ -102,16 +84,16 @@ void STRING_ADD(string* s, const char* str) {
 	*s = s_data->buff;
 }
 
-void STRING_INSERT(string* s, str_size pos, const char* str) {
-	string_data* s_data = STRING_DATA(*s);
+void string_insert(string* s, size_t pos, const char* str) {
+	string_data* s_data = string_get_data(*s);
 	
-	str_size str_length = strlen(str);
+	size_t str_length = strlen(str);
 	
-	str_size new_length = s_data->length + str_length;
+	size_t new_length = s_data->length + str_length;
 	
 	// make sure there is enough room for new characters and null terminator
 	if (s_data->alloc <= new_length + 1) {
-		string_data* new_s_data = STRING_ALLOC(new_length);
+		string_data* new_s_data = string_alloc(new_length);
 		
 		memcpy(new_s_data->buff, s_data->buff, pos); // copy leading characters
 		memcpy(&new_s_data->buff[pos+str_length], &s_data->buff[pos], s_data->length-pos); // copy trailing characters
@@ -133,16 +115,16 @@ void STRING_INSERT(string* s, str_size pos, const char* str) {
 	*s = s_data->buff;
 }
 
-void STRING_REPLACE(string* s, str_size pos, str_size len, const char* str) {
-	string_data* s_data = STRING_DATA(*s);
+void string_replace(string* s, size_t pos, size_t len, const char* str) {
+	string_data* s_data = string_get_data(*s);
 	
-	str_size str_length = strlen(str);
+	size_t str_length = strlen(str);
 	
-	str_size new_length = s_data->length + str_length - len;
+	size_t new_length = s_data->length + str_length - len;
 	
 	// make sure there is enough room for new characters and null terminator
 	if (s_data->alloc <= new_length + 1) {
-		string_data* new_s_data = STRING_ALLOC(new_length);
+		string_data* new_s_data = string_alloc(new_length);
 		
 		memcpy(new_s_data->buff, s_data->buff, pos); // copy leading characters
 		memcpy(&new_s_data->buff[pos+str_length], &s_data->buff[pos+len], s_data->length-pos-len); // copy trailing characters
@@ -165,22 +147,22 @@ void STRING_REPLACE(string* s, str_size pos, str_size len, const char* str) {
 	*s = s_data->buff;
 }
 
-void STRING_REMOVE(string s, str_size pos, str_size len) {
-	string_data* s_data = STRING_DATA(s);
+void string_remove(string s, size_t pos, size_t len) {
+	string_data* s_data = string_get_data(s);
 	// anyone who puts in a bad index can face the consequences on their own
 	memmove(&s_data->buff[pos], &s_data->buff[pos+len], s_data->length - pos);
 	s_data->length -= len;
 	s_data->buff[s_data->length] = '\0';
 }
 
-void STRING_FREE(string s) {
-	free(STRING_DATA(s));
+void string_free(string s) {
+	free(string_get_data(s));
 }
 
-str_size STRING_SIZE(string s) {
-	return ((str_size*)s)[-1];
+size_t string_size(string s) {
+	return ((size_t*)s)[-1];
 }
 
-str_size STRING_GET_ALLOC(string s) {
-	return ((str_size*)s)[-2];
+size_t string_get_alloc(string s) {
+	return ((size_t*)s)[-2];
 }
