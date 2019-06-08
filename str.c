e3ea8295f08392677d9746a5f32a0b1d79fff1c7
Mashpoe <mashpoesemail@gmail.com>
2019-06-08 16:12:45 -0600

    added str_size alias again
    this makes it easier to change the size of string data structures

--- str.c
+++ str.c
@@ -11,12 +11,12 @@
 typedef struct string_data string_data;
 
 struct string_data {
-	size_t alloc; // stores the number of bytes allocated
-	size_t length;
+	str_size alloc; // stores the number of bytes allocated
+	str_size length;
 	char buff[];
 };
 
-string_data* string_alloc(size_t length) {
+string_data* string_alloc(str_size length) {
 	string_data* s_data = malloc(sizeof(string_data) + length + 1); // plus 1 for null terminator
 	s_data->alloc = length+1;
 	s_data->length = length;
@@ -31,7 +31,7 @@ string string_create(const char* str) {
 	string_data* s_data = NULL;
 	
 	if (str != NULL) {
-		size_t str_length = strlen(str);
+		str_size str_length = strlen(str);
 		s_data = string_alloc(str_length);
 		memcpy(&s_data->buff, str, str_length);
 		s_data->buff[str_length] = '\0';
@@ -52,7 +52,7 @@ void string_add_char(string* s, char c) {
 	string_data* s_data = string_get_data(*s);
 	
 	if (!string_has_space(s_data)) {
-		size_t new_alloc = s_data->alloc * 2;
+		str_size new_alloc = s_data->alloc * 2;
 		s_data = realloc(s_data, sizeof(string_data) + new_alloc);
 		s_data->alloc = new_alloc;
 	}
@@ -65,9 +65,9 @@ void string_add_char(string* s, char c) {
 void string_add(string* s, const char* str) {
 	string_data* s_data = string_get_data(*s);
 	
-	size_t str_length = strlen(str);
+	str_size str_length = strlen(str);
 	
-	size_t new_length = s_data->length + str_length;
+	str_size new_length = s_data->length + str_length;
 	
 	// make sure there is enough room for new characters and null terminator
 	if (s_data->alloc <= new_length + 1) {
@@ -84,12 +84,12 @@ void string_add(string* s, const char* str) {
 	*s = s_data->buff;
 }
 
-void string_insert(string* s, size_t pos, const char* str) {
+void string_insert(string* s, str_size pos, const char* str) {
 	string_data* s_data = string_get_data(*s);
 	
-	size_t str_length = strlen(str);
+	str_size str_length = strlen(str);
 	
-	size_t new_length = s_data->length + str_length;
+	str_size new_length = s_data->length + str_length;
 	
 	// make sure there is enough room for new characters and null terminator
 	if (s_data->alloc <= new_length + 1) {
@@ -115,12 +115,12 @@ void string_insert(string* s, size_t pos, const char* str) {
 	*s = s_data->buff;
 }
 
-void string_replace(string* s, size_t pos, size_t len, const char* str) {
+void string_replace(string* s, str_size pos, str_size len, const char* str) {
 	string_data* s_data = string_get_data(*s);
 	
-	size_t str_length = strlen(str);
+	str_size str_length = strlen(str);
 	
-	size_t new_length = s_data->length + str_length - len;
+	str_size new_length = s_data->length + str_length - len;
 	
 	// make sure there is enough room for new characters and null terminator
 	if (s_data->alloc <= new_length + 1) {
@@ -147,7 +147,7 @@ void string_replace(string* s, size_t pos, size_t len, const char* str) {
 	*s = s_data->buff;
 }
 
-void string_remove(string s, size_t pos, size_t len) {
+void string_remove(string s, str_size pos, str_size len) {
 	string_data* s_data = string_get_data(s);
 	// anyone who puts in a bad index can face the consequences on their own
 	memmove(&s_data->buff[pos], &s_data->buff[pos+len], s_data->length - pos);
@@ -159,10 +159,10 @@ void string_free(string s) {
 	free(string_get_data(s));
 }
 
-size_t string_size(string s) {
-	return ((size_t*)s)[-1];
+str_size string_size(string s) {
+	return ((str_size*)s)[-1];
 }
 
-size_t string_get_alloc(string s) {
-	return ((size_t*)s)[-2];
+str_size string_get_alloc(string s) {
+	return ((str_size*)s)[-2];
 }
